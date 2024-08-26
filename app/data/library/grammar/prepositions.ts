2a7a69bdc815a531c697a5aec6ade740ead401af
Ben Chu <kt7044benchu003@gmail.com>
2024-08-26 19:58:44 +0800

    small changes

--- app/data/library/grammar/prepositions.ts
+++ app/data/library/grammar/prepositions.ts
@@ -4,125 +4,110 @@ const prepositions = [
         content: [
             {
                 topic: "time",
-                example: "e.g. We will meet [at] 3 pm."
+                example: "e.g. We will meet [at] 3 pm.",
             },
             {
                 topic: "place",
-                example: "e.g. The book is [on] the table."
+                example: "e.g. The book is [on] the table.",
             },
             {
                 topic: "movement",
-                example: "e.g. She went [to] the park."
+                example: "e.g. She went [to] the park.",
             },
             {
                 topic: "position",
-                example: "e.g. The cat is [under] the bed."
+                example: "e.g. The cat is [under] the bed.",
             },
             {
                 topic: "cause",
-                example: "e.g. He was late [because of] traffic."
+                example: "e.g. He was late [because of] traffic.",
             },
             {
                 topic: "purpose",
-                example: "e.g. She went to the shop [for] some bread."
+                example: "e.g. She went to the shop [for] some bread.",
             },
             {
                 topic: "manner",
-                example: "e.g. She sat there [in silence]."
-            }
+                example: "e.g. She sat there [in silence].",
+            },
         ],
-        list:[
-            "time",
-            "place",
-            "movement",
-            "position",
-            "cause",
-            "purpose",
-            "manner"
-        ],
+        list: ["time", "place", "movement", "position", "cause", "purpose", "manner"],
     },
     {
         name: "Prepositional Phrases",
         content: [
             {
                 topic: "adjectives: primary level",
-                example: "e.g. He is [angry at] me."
+                example: "e.g. He is [angry at] me.",
             },
             {
                 topic: "adjectives: secondary level",
-                example: "e.g. He is [fond of] chocolate."
+                example: "e.g. He is [fond of] chocolate.",
             },
             {
                 topic: "made + preposition",
-                example: "e.g. This is [made in] Hong Kong."
+                example: "e.g. This is [made in] Hong Kong.",
             },
             {
                 topic: "good/bad + preposition",
-                example: "e.g. This is [for] your health."
+                example: "e.g. This is [for] your health.",
             },
             {
                 topic: "give directions",
-                example: "e.g. The shop is [next to] the bank."
+                example: "e.g. The shop is [next to] the bank.",
             },
         ],
-        list:[
-            "adjectives: primary level",
-            "adjectives: secondary level",
-            "made + preposition",
-            "good/bad + preposition",
-            "give directions",
-
-        ],
+        list: ["adjectives: primary level", "adjectives: secondary level", "made + preposition", "good/bad + preposition", "give directions"],
     },
     {
-        name: "Combinations",
+        name: "Preposition Comparison",
         content: [
             {
                 topic: "in vs with (clothing)",
-                example: "e.g. She's [in] a dress vs. She's [with] a scarf."
+                example: "e.g. She's [in] a dress vs. She's [with] a scarf.",
             },
             {
                 topic: "in vs on (vehicles)",
-                example: "e.g. I'm [in] a car vs. I'm [on] a bus."
+                example: "e.g. I'm [in] a car vs. I'm [on] a bus.",
             },
             {
                 topic: "by vs before",
-                example: "e.g. I'll do it [by] Friday vs. I'll do it [before] 5 pm."
+                example: "e.g. I'll do it [by] Friday vs. I'll do it [before] 5 pm.",
             },
             {
                 topic: "until, before and after",
-                example: "e.g. Stay [until] I return."
+                example: "e.g. Stay [until] I return.",
             },
             {
                 topic: "during vs in",
-                example: "e.g. She called [during] the meeting."
+                example: "e.g. She called [during] the meeting.",
             },
             {
                 topic: "between vs among",
-                example: "e.g. The secret is [between] you and me vs. The gift will be divided [among] the three kids."
+                example: "e.g. The secret is [between] you and me vs. The gift will be divided [among] the three kids.",
             },
             {
                 topic: "beside vs besides",
-                example: "e.g. She sat [beside] me vs. [Besides] math, I also have English homework."
+                example: "e.g. She sat [beside] me vs. [Besides] math, I also have English homework.",
             },
             {
                 topic: "in vs into",
-                example: "e.g. She is [in] the room vs. She walked [into] the room."
+                example: "e.g. She is [in] the room vs. She walked [into] the room.",
             },
             {
                 topic: "of vs off",
-                example: "e.g. Cup [of] tea vs. Take your shoes [off]."
+                example: "e.g. Cup [of] tea vs. Take your shoes [off].",
             },
             {
                 topic: "amid vs among",
-                example: "e.g. The house stands [amid] the trees."
+                example: "e.g. The house stands [amid] the trees.",
             },
             {
                 topic: "about vs around",
-                example: "e.g. I'll be there [around] 5 pm."
+                example: "e.g. I'll be there [around] 5 pm.",
             },
         ],
-        list:[
+        list: [
             "in vs with (clothing)",
             "in vs on (vehicles)",
             "by vs before",
@@ -135,7 +120,7 @@ const prepositions = [
             "amid vs among",
             "about vs around",
         ],
-    }
-]
+    },
+];
 
 export default prepositions;
